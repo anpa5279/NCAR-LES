f8542c2f76849ba0e73b300770d3e6930b4f9a02
anpa5279 <anpa5279@colorado.edu>
2025-06-02 12:00:16 -0600

    ensuring variables are updated and the boundaries do not get weird.

--- source/diff/smag_vis.f90
+++ source/diff/smag_vis.f90
@@ -11,6 +11,14 @@ SUBROUTINE smag_vis(alk)
   DO iz=izs-1,ize+1
     izp1 = iz + 1
     izm1 = iz - 1
+    dslk  = dsl_z(iz)
+    IF(iz .gt. 0) dslk  = AMIN1(dsl_z(iz),vk*ABS(z(iz))/csmag)
+    ! NO STABILITY CORRECTED LENGTH SCALES
+    DO j=iys,iye
+      DO i=1,nnx
+        alk(i,j,iz) = dslk
+      END DO
+    END DO
     ! GET FLUCTUATING STRAINS
     DO j=iys,iye
       DO i=1,nnx
@@ -22,11 +30,22 @@ SUBROUTINE smag_vis(alk)
         s13 =  (0.5 * ((((u(i,j,izp1) - u(i,j,iz)) * dzu_i(izp1) + wx(i,j,iz)))))**2
         s23 =  (0.5 * ((v(i,j,izp1) - v(i,j,iz)) * dzu_i(izp1) + wy(i,j,iz)))**2
         sij2(i,j,iz) = s11 + s22 + s33 + 2 * s12 + 2 * s13 + 2 * s23
-        vis_m(i,j,iz)  = (csmag*dsl_z(iz))**2 * SQRT(2 * sij2(i, j, iz))
+        vis_m(i,j,iz)  = (csmag*dslk)**2 * SQRT(2 * sij2(i, j, iz))
         vis_s(i,j,iz)  = vis_m(i,j,iz)
         vis_sv(i,j,iz) = vis_s(i,j,iz)
       ENDDO
     ENDDO
+
+    ! SPECIAL CASE FOR IZ = 1
+    IF(iz==1 .AND. ibcl == 0) THEN
+      DO iy=iys,iye
+        DO ix=1,nnx
+          vis_m(ix,iy,iz-1)  = vis_m(ix,iy,iz)
+          vis_s(ix,iy,iz-1)  = vis_s(ix,iy,iz)
+          vis_sv(ix,iy,iz-1) = vis_sv(ix,iy,iz)
+        ENDDO
+      ENDDO
+    ENDIF
   ENDDO
 
   RETURN
