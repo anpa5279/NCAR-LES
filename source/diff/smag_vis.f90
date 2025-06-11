16b628976f1ec415836862ecadd043af9b3673a7
anpa5279 <anpa5279@colorado.edu>
2025-06-11 17:31:46 -0600

    smagorinsky is the problem. adding print s tatements.

--- source/diff/smag_vis.f90
+++ source/diff/smag_vis.f90
@@ -10,7 +10,6 @@ SUBROUTINE smag_vis(alk)
   REAL alk(nnx,iys:iye,izs-1:ize+1)
   DO iz=izs-1,ize+1
     izp1 = iz + 1
-    izm1 = iz - 1
     dslk  = dsl_z(iz)
     IF(iz .gt. 0) dslk  = AMIN1(dsl_z(iz),vk*ABS(z(iz))/csmag)
     ! NO STABILITY CORRECTED LENGTH SCALES
@@ -19,9 +18,6 @@ SUBROUTINE smag_vis(alk)
         alk(i,j,iz) = dslk
       END DO
     END DO
-    IF (dzw_i(izp1) == 0.0 .OR. dzu_i(izp1) == 0.0) THEN
-        print*, 'Zero denominator at iz=', iz, ' -> dzw_i(izp1)=', dzw_i(izp1), ' dzu_i(izp1)=', dzu_i(izp1)
-    ENDIF
     ! GET STRAINS and compute viscosity
     DO j=iys,iye
       DO i=1,nnx
@@ -32,14 +28,14 @@ SUBROUTINE smag_vis(alk)
         s12 =  (0.5 * (uy(i,j,iz) + vx(i,j,iz)))**2
         s13 =  (0.5 * ((((u(i,j,izp1) - u(i,j,iz)) * dzu_i(izp1) + wx(i,j,iz)))))**2
         s23 =  (0.5 * ((v(i,j,izp1) - v(i,j,iz)) * dzu_i(izp1) + wy(i,j,iz)))**2
-        print *, "s11", s11, "s22", s22, "s33", s33, "s12", s12, "s13", s13, "s23", s23
         sij2(i,j,iz) = s11 + s22 + s33 + 2 * s12 + 2 * s13 + 2 * s23
         vis_m(i,j,iz)  = (csmag*dslk)**2 * SQRT(2 * sij2(i, j, iz))
         vis_s(i,j,iz)  = vis_m(i,j,iz)
         vis_sv(i,j,iz) = vis_s(i,j,iz)
       ENDDO
     ENDDO
-    print*, "iz", iz, "vis_m", vis_m(1,1,iz), "vis_s", vis_s(1,1,iz), "vis_sv", vis_sv(1,1,iz), "dslk", dslk, "sij2", sij2(1,1,iz)
+    print*, "iz", iz, "dzw_i(izp1)", dzw_i(izp1)
+    print*, "vis_m", vis_m(1,1,iz), "vis_s", vis_s(1,1,iz), "vis_sv", vis_sv(1,1,iz), "dslk", dslk, "sij2", sij2(1,1,iz)
     ! SPECIAL CASE FOR IZ = 1
     IF(iz==1 .AND. ibcl == 0) THEN
       DO iy=iys,iye
