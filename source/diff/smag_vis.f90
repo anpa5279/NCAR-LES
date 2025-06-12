8b5216fc47980f1a76650349d0824da6a679e073
anpa5279 <anpa5279@colorado.edu>
2025-06-11 19:59:44 -0600

    removing print statements

--- source/diff/smag_vis.f90
+++ source/diff/smag_vis.f90
@@ -50,8 +50,8 @@ SUBROUTINE smag_vis(alk)
         vis_sv(i,j,iz) = vis_s(i,j,iz)
       ENDDO
     ENDDO
-    print*, "iz", iz, "csmag", csmag! "dzw_i(izp1)", dzw_i(izp1), "dzu_i(izp1)", dzu_i(izp1)
-    print*, "vis_m", vis_m(1,1,iz), "vis_s", vis_s(1,1,iz), "dslk", dslk, "sij2", sij2(1,1,iz)
+    !print*, "iz", iz, "csmag", csmag! "dzw_i(izp1)", dzw_i(izp1), "dzu_i(izp1)", dzu_i(izp1)
+    !print*, "vis_m", vis_m(1,1,iz), "vis_s", vis_s(1,1,iz), "dslk", dslk, "sij2", sij2(1,1,iz)
     ! SPECIAL CASE FOR IZ = 1
     IF(iz==1 .AND. ibcl == 0) THEN
       DO iy=iys,iye
