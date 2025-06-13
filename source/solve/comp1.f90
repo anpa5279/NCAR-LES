4fc62c5efc75b1394344e4a210461d796c301efb
anpa5279 <anpa5279@colorado.edu>
2025-06-13 10:30:26 -0600

    fixed if statement

--- source/solve/comp1.f90
+++ source/solve/comp1.f90
@@ -44,7 +44,7 @@ SUBROUTINE comp1(istep,it)
   ENDDO
 
   ! GET VISCOSITY AND RHS OF (E,U,V,W) EQUATIONS AT NEXT STEP
-  IF i_dear == 2 THEN
+  IF (i_dear == 2) THEN
     CALL smag_vis
   ELSE
     CALL tke_vis(istep)
