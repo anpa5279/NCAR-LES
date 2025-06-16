1d52e675d292f6b17636525694936611f967487e
anpa5279 <anpa5279@colorado.edu>
2025-06-16 12:16:17 -0600

    reverted print.f and reduce grid size

--- source/save/print.f
+++ source/save/print.f
@@ -39,12 +39,34 @@ c
      +              wtsb(iz,1),shrz(iz),buyz(iz)
  4300    format(1X,I3,e12.4,7e12.4)
 19999 continue
+      write(lu,4400)tsfcc(1),wtsfc(1)
+ 4400 format('  SURFACE VALUE: TXYM=',F8.2,'               WTSB=',E9.2)
+
+         write(lu,4500) stokess,udrift,vdrift
+ 4500    format(/,' STOKESS = ',e12.4,' UDRIFT = ',e12.4,
+     +          ' VDRIFT = ',e12.4)
 
       write(lu,4600) (iz,uxym(iz)+ugal,vxym(iz),uwle(iz),
      +       uwsb(iz),vwle(iz),vwsb(iz),iz=iz_strt,iz_end)
  4600 format(//,' IZ',5x,' UXYM + UGAL',8x,' VXYM',10x,' UWLE',10x,
      +          ' UWSB',10x,' VWLE',10x,' VWSB'
      +       ,/,(1x,i4,6(3x,e13.6)))
+      if(ivis .eq. 1) then
+        write(lu,4800) xksurf, nmatch, viscon, vise
+ 4800   format(//,' XKSURF = ',e15.6,' NMATCH = ',i4,/,
+     +             ' VISCON = ',e15.6,' VISE = ',e15.6)
+        write(lu,4700) (iz,dfac(iz),iz=iz_strt,iz_end)
+ 4700   format(//,'   IZ',5x,'  DFAC',/,(1x,i4,3x,e15.6))
+      endif
+      if (flg_reaction .eq. 1) then
+        write(lu,5100) (iz,txym(iz,2),txym(iz,3),
+     +              txym(iz,4),txym(iz,5),txym(iz,6),
+     +              txym(iz,7),iz=iz_strt,iz_end)
+ 5100   format(//,' IZ',5x,' SCALAR-1 MEAN',8x,'2-MEAN',10x,
+     +          '3-MEAN',10x, '4-MEAN', 10x, '5-MEAN',10x,
+     +          '6-MEAN',10x
+     +       ,/,(1x,i4,6(3x,e13.6)))
+      endif 
 
       return
       end
