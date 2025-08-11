45241d0be50642594834a77ad63f77604cb600cb
anpa5279 <anpa5279@colorado.edu>
2025-08-11 17:48:52 -0600

    test smag sgs model aprt 7, changed how the smag function is called

--- source/solve/comp1.f90
+++ source/solve/comp1.f90
@@ -44,14 +44,15 @@ SUBROUTINE comp1(istep,it)
   ENDDO
 
   ! GET VISCOSITY AND RHS OF (E,U,V,W) EQUATIONS AT NEXT STEP
-  IF (i_dear == 2) THEN 
-    CALL smag_vis(istep)
-    DO iz=izs-1,ize
-      vis_mean(iz) = 0.0
-    ENDDO
-  ELSE 
-    CALL tke_vis(istep)
-  ENDIF
+  !IF (i_dear == 2) THEN 
+  !  CALL smag_vis(istep)
+  !  DO iz=izs-1,ize
+  !    vis_mean(iz) = 0.0
+  !  ENDDO
+  !ELSE 
+  !  CALL tke_vis(istep)
+  !ENDIF
+  CALL tke_vis(istep)
   CALL rhs_uvw(istep)
 
   ! EVALUATE RHS OF SCALAR EQUATIONS=
