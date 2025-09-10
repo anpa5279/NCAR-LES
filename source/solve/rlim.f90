62cf20f2c4b7164d558cd655acded1fc88638217
anpa5279 <anpa5279@colorado.edu>
2025-09-10 15:16:15 -0600

    updating init

--- source/solve/rlim.f90
+++ source/solve/rlim.f90
@@ -1,7 +1,7 @@
 FUNCTION rlim(d1, d2, d3)
 ! CEE'S KAPPA = 1/3 SCHEME
-    r = sign(1e-15, d2 - d3) + (d2 - d3)
-    r = (d1 - d2) / r
+
+    r = (d1 - d2 + 1.e-100) / (d2 - d3 - 1.e-100)
     rlim = (d2 - d3) * AMAX1(0., AMIN1(r, AMIN1(1./6.+1./3.*r, 1.)))
 
     RETURN
