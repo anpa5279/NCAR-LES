f816c2ef681b6f89ffe3a8fab599395fedc561cc
anpa5279 <anpa5279@colorado.edu>
2025-09-10 15:16:15 -0600

    updating init

--- source/init/init.f90
+++ source/init/init.f90
@@ -86,11 +86,6 @@ SUBROUTINE init
     sigma_p = pi2 * f_p
     grav_w = grav
 
-    ! RATIO OF K_1/K_P (PHILLIPS AND DONELAN)
-    r_kp = grav * (f2w * pi2 / ustokes)**2
-    r_k1 = r_fac * grav / (cd_10 * ustokes * ustokes)
-    rk_ratio = r_k1 / r_kp
-
     time = 0.0
 
     ! OUTERMOST COARSE GRID INDICES ARE BOUNDS OF GRID
--- source/solve/rlim.f90
+++ source/solve/rlim.f90
@@ -1,7 +1,7 @@
 FUNCTION rlim(d1, d2, d3)
 ! CEE'S KAPPA = 1/3 SCHEME
 
-    r = (d1 - d2 + 1.e-100) / (d2 - d3 - 1.e-100)
+    r = (d1 - d2) / (d2 - d3 )
     rlim = (d2 - d3) * AMAX1(0., AMIN1(r, AMIN1(1./6.+1./3.*r, 1.)))
 
     RETURN
