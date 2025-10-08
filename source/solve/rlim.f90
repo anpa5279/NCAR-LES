3daa48ab78caf981f3b38cb7b7ef941d1ff81c63
anpa5279 <anpa5279@colorado.edu>
2025-10-08 15:48:43 -0600

    rlim-1 faster run time

--- source/solve/rlim.f90
+++ source/solve/rlim.f90
@@ -5,7 +5,7 @@ FUNCTION rlim(d1, d2, d3)
 
     REAL :: tol = 1.e-20
     r = sign(tol, d2 - d3) + (d2 - d3)
-    r = (d1 - d2) / r
+    r = (d1 - d2 - tol) / r
     !r = (d1 - d2 + 1.e-100) / (d2 - d3 - 1.e-100)
     rlim = (d2 - d3) * AMAX1(0., AMIN1(r, AMIN1(1./6. + 1./3. * r, 1.)))
 
