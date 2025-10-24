0aba8cf0652d8a9df843f80c6518f1be737019be
anpa5279 <anpa5279@colorado.edu>
2025-10-24 10:01:49 -0600

    reverting to previous for a sanity check

--- source/diff/smag_vis.f90
+++ source/diff/smag_vis.f90
@@ -1,5 +1,6 @@
 SUBROUTINE smag_vis(istep)
-    !! Classic Smagorinsky LES model.
+! Classic Smagorinsky LES model.
+! WARNING: assumes ivis0 == 0 (set in pars.f90).
 
     USE pars
     USE fields
@@ -8,137 +9,99 @@ SUBROUTINE smag_vis(istep)
 
     IMPLICIT NONE
 
-    REAL :: wz, uz, vz, s11, s22, s33, s12, s13, s23, sij2, delta
-    REAL :: d_im, d_ip, d_i, wt_l, wt_h
+    REAL :: wz, uzp, vzp, wzp, s11, s22, s33, s12, s13, s23, sij2, weit, weit1, d_grid
     INTEGER :: istep, iz, i, j, izm1, izp1
 
-    ! Step 0: Change the Lower BC for u and v
-    ! -------
-    ! We want zero vertical SGS flux across the boundaries, which requires that
-    ! uz = vz = 0.0 at the z-face pt, which affects the gradient computation at
-    ! the first interior z-center pt for u and v. Therefore override the BCs set
-    ! by Lower.f90 for right now.
-    ! I believe Lower.f90 sets u = v = 0 for an atmospheric BL still,
-    ! instead of uz = vz = 0 for an oceanic BL, which no one caught because it
-    ! didn't matter when using a staggered vertical grid and one-equation SGS
-    ! model. I could be wrong though, and maybe that BC is correct, and I need
-    ! to rethink this...
-    !IF (izs == 1) THEN
-    !    u(:, :, 0) = u(:, :, 1)
-    !    v(:, :, 0) = v(:, :, 1)
-    !END IF
-
-    ! Step 1: Compute SGS viscosity at z-center interior points
-    ! -------
-    ! NOTE: z-center points are always halfway between z-face points, therefore, to
-    ! interpolate from face to center, it's a simple (equal-weighted) average.
+    ! Index (i, j, izs-2) is out of bounds when computing wz for izs-1, and (i, j, ize+2) is out
+    ! out bounds when computing uzp, vzp, wzp for ize+1. Therefore, we must supply vis_x values
+    ! for izs-1 and ize+1 separately, regardless of whether izs=1 or ize=nnz (BCs). This is simply
+    ! a halo-cell inadequacy in NCARLES for computing Smagorinsky viscosity, and it implies that
+    ! the solution will change as the MPI decomposition is changed (i.e., solution becomes
+    ! dependent on values of ncpu_s and numprocs), which is not good.
     DO iz = izs, ize
         izm1 = iz - 1
         izp1 = iz + 1
-        delta = dsl_z(iz)
-
-        ! 2nd-order finite difference coefficients for z-center gradient from
-        ! z-center data on a non-uniform grid
-        d_im = 1.0 / (dzw(iz) + dzw(izm1)) ! = 1 / 2dz on uniform grid
-        d_ip = 1.0 / (dzw(izp1) + dzw(iz)) ! = 1 / 2dz on uniform grid
-        d_i = d_im - d_ip                  ! = 0 on uniform grid
+        weit = dzw(iz) / (dzw(iz) + dzw(izp1))
+        weit1 = 1.0 - weit
+        d_grid = dsl_z(iz) ! dsl_z gives correct SGS length scale given dealiased x and y derivatives
 
         DO j = iys, iye
             DO i = 1, nnx
-                ! Calculate vertical gradients at z-center points
+                ! Calculate vertical gradients.
                 wz = (w(i, j, iz) - w(i, j, izm1)) * dzw_i(iz)
-                uz = d_ip * u(1, j, izp1) + d_i * u(1, j, iz) - d_im * u(1, j, izm1)
-                vz = d_ip * v(1, j, izp1) + d_i * v(1, j, iz) - d_im * v(1, j, izm1)
+                uzp = (u(i, j, izp1) - u(i, j, iz)) * dzu_i(izp1)
+                vzp = (v(i, j, izp1) - v(i, j, iz)) * dzu_i(izp1)
+                wzp = (w(i, j, izp1) - w(i, j, iz)) * dzw_i(izp1)
 
-                ! Strain rate tensor terms squared, sij = [1/2*(u_<i,j> + u_<j,i>)]
-                s11 = ux(i, j, iz)**2
-                s22 = vy(i, j, iz)**2
-                s33 = wz**2
-                s12 = 0.25 * (uy(i, j, iz) + vx(i, j, iz))**2
-                s13 = 0.25 * (uz + 0.5 * (wx(i, j, iz) + wx(i, j, izm1)))**2
-                s23 = 0.25 * (vz + 0.5 * (wy(i, j, iz) + wy(i, j, izm1)))**2
+                ! Strain rate tensor terms squared. strain rate tens: sij= 1/2*(u_<i,j> + u_<j,i>)
+                s11 = weit1 * ux(i, j, iz)**2 + weit * ux(i, j, izp1)**2
+                s22 = weit1 * vy(i, j, iz)**2 + weit * vy(i, j, izp1)**2
+                s33 = weit * wzp**2 + weit1 * wz**2
+                s12 = weit1 * (0.5 * (uy(i, j, iz) + vx(i, j, iz)))**2 &
+                      + weit * (0.5 * (uy(i, j, izp1) + vx(i, j, izp1)))**2
+
+                s13 = (0.5 * (uzp + wx(i, j, iz)))**2
+                s23 = (0.5 * (vzp + wy(i, j, iz)))**2
+
                 sij2 = s11 + s22 + s33 + 2.0 * (s12 + s13 + s23)
-
-                ! using vis_sv to store z-center viscosity values
-                vis_sv(i, j, iz) = (csmag * delta)**2 * SQRT(2.0 * sij2)
+                vis_m(i, j, iz) = (csmag * d_grid)**2 * SQRT(2.0 * sij2)
+                vis_s(i, j, iz) = 3.0 * vis_m(i, j, iz) ! 1 + 2 * (alk/dslk) = 3 for non-stability-corrected length scales
+                vis_sv(i, j, iz) = vis_s(i, j, iz)
             END DO
         END DO
     END DO
 
-    ! Step 2: Interpolate to z-face points, and add appropriate BCs
-    ! -------
-    call smag_exchange ! fills halo cells just for vis_sv
-
-    ! Step 3: Interpolate to z-face points
-    ! -------
-    DO iz = izs, ize
-        izp1 = iz + 1
-        wt_h = dzw(iz) / (dzw(iz) + dzw(izp1))
-        wt_l = 1.0 - wt_h
-
+    ! Lower BC sets these stored values @iz=0:
+    !   r3 = 0.0
+    !   ux = uy = vx = vy = wx = wy = 0
+    !   tau_13 = wind stress from SUFTO
+    !   tau_23 = wind stress from SUFTO
+    ! Therefore, tau_11 = tau_22 = tau_12 = 0 identically, tau_33 is ignored (r3 = 0), and tau_13
+    ! and tau_23 do not require vis_m. However, the vis_x @iz=0 are added to vis_x @iz=1 to
+    ! compute tau_11, tau_12, tau_22, and the tau_t's @iz=1. Therefore the best thing to do is set
+    ! them equal to the vis_x @iz=1, as in the TKE_VIS routines.
+    IF (izs == 1) THEN
         DO j = iys, iye
             DO i = 1, nnx
-                vis_m(i, j, iz) = wt_l * vis_sv(i, j, iz) + wt_h * vis_sv(i, j, izp1)
+                vis_m(i, j, izs - 1) = vis_m(i, j, izs)
+                vis_s(i, j, izs - 1) = vis_s(i, j, izs)
+                vis_sv(i, j, izs - 1) = vis_sv(i, j, izs)
             END DO
         END DO
-    END DO
-
-    ! Step 4: Set the Boundary Conditions
-    ! -------
-    ! Lower BC (air-sea interface):
-    ! No SGS diffusion of quantities across interface
-    ! -- RHS_X override tau13, tau23, and taut3 with only the flux BC
-    !    and ignore SGS stresses, which covers this requirement.
-    !
-    ! Horizontal SGS diffusion at first internal point should depend only on the
-    ! viscosity at that internal point.
-    ! -- This requires that when RHS_X averages the face-located vis_x, the
-    !    result is exactly what was computed in Step 1 for the center-located
-    !    vis_x
-    !    vis_c(1) = 0.5 * (vis_f(0) + vis_f(1))
-    !    -> vis_f(0) = 2.0 * vis_c(1) - vis_f(1)
-    ! -- This is different than TKE-based model, which sets vis_f(0) = vis_f(1),
-    !    resulting in vis_c(1) = vis_f(1), which isn't perfectly accurate.
-    IF (izs == 1) THEN
-        vis_m(:, :, izs-1) = 2.0 * vis_sv(:, :, izs) - vis_m(:, :, izs)
     END IF
 
-    ! The Upper BC isn't exactly a reflection/slip wall BC.
-    ! Temperature (and scalars) can have a non-zero gradient. The TKE-based
-    ! SGS model relies on e(nnz) = 0.0 to zero out all SGS diffusion
-    ! at the boundary, which distorts horizontal SGS diffusion at the last
-    ! interior point for u, v, and t. Here we have the option to better match
-    ! velocities and scalars separately than in the TKE-based model.
-    !
-    ! No vertical SGS diffusion
-    ! -- tau13 = tau23 = 0 at face-pt nnz because all velocity gradients are
-    !    already 0. But, dT/dz is not 0, so we need to make vis_sv = 0.
-    !
-    ! Properly-matched horizontal SGS diffusion
-    ! -- same procedure as above for vis_m and vis_s
+    ! Upper BC sets these stored values @iz=nnz+1:
+    !   u = u(nnz) => implies `uzp` @(nnz:nnz+1) = 0
+    !   v = v(nnz) => implies `vzp` @(nnz:nnz+1) = 0
+    !   ux = uy = vx = vy = wx = wy = 0
+    ! and @iz=(nnz:nnz+1) sets these values:
+    !   w = 0.0 => implies `wzp` @(nnz) = 0, `wz` @(nnz+1) = 0
+    !   r3 = 0.0.
+    ! Therefore, all sij = 0.0 @nnz+1 identically.
+    ! This means the tau's and vis_x's should all be 0.0 @ nnz+1. In fact, the
+    ! vis_s/sv(ize+1) index is not even used in RHS_SCL, and thefore isn't needed.
+    ! However, vis_m(ize+1) is only used in RHS_UVW
+    ! where, if ize=nnz, the result is overwritten by r3 = 0.0. Therefore, we can just let whatever
+    ! value got computed for ize+1 in the main DO loop above be left alone, as it does not
+    ! computationally matter, just like in the TKE_VIS routines.
     IF (ize == nnz) THEN
-        vis_m(:, :, ize) = 2.0 * vis_sv(:, :, ize) - vis_m(:, :, ize-1)
-        vis_s(:, :, ize) = 3.0 * vis_m(:, :, ize)
-        vis_sv(:, :, ize) = 0.0
-    ELSE
-        vis_s(:, :, ize) = 3.0 * vis_m(:, :, ize)
-        vis_sv(:, :, ize) = vis_s(:, :, ize)
+        DO j = iys, iye
+            DO i = 1, nnx
+                vis_m(i, j, ize + 1) = 0.0
+                vis_s(i, j, ize + 1) = 0.0
+                vis_sv(i, j, ize + 1) = 0.0
+            END DO
+        END DO
     END IF
 
-    ! Step 5: Update the rest of vis_s and vis_sv
-    ! -------
-    vis_s(:, :, izs-1:ize-1) = 3.0 * vis_m(:, :, izs-1:ize-1)
-    vis_sv(:, :, izs-1:ize-1) = vis_s(:, :, izs-1:ize-1)
-
-    ! Step 6: Revert the change to the Lower BC for u and v
-    ! -------
-    ! I'm not willing to bet my life that there isn't something somewhere in the
-    ! code that needs the u = v = 0 BC to be correct, which is why we're keeping
-    ! Lower.f90 as-is and doing this thing.
-    !IF (izs == 1) THEN
-    !    u(:, :, 0) = ubc(:, :, 2)
-    !    v(:, :, 0) = vbc(:, :, 2)
-    !END IF
+    ! SIDE NOTE: The unexpected Dirichlet BCs for u and v set in lower.f90 are irrelevant to
+    ! rhs_uvw, as the gradients ux, uy, vx, vy, as well as the stresses tau_13 and tau_23, are all
+    ! set separately. Therefore, only uz and vz are set by the u and v BC, and because of the
+    ! staggered vertical grid, both of these gradients only appear advectively, multiplied by the
+    ! w = 0.0 condition. My guess is this setup is a leftover from the old atmospheric physics.
+    ! My expectation was that the "rigid lid" surface condition would be a free-slip wall, and
+    ! therefore u and v would have gradient conditions (equaling wind-stress for u, zero for v).
+    ! I don't know what ugal or ugcont are, but they are set to 0.0 in the code.
 
     RETURN
 END SUBROUTINE
