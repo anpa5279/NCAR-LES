707970c5c9bd629e4ecc750d317b87353f770f23
anpa5279 <anpa5279@colorado.edu>
2025-11-12 10:20:29 -0700

    perturbed carbon dioxide

--- source/main/inputs.f90
+++ source/main/inputs.f90
@@ -8,7 +8,7 @@ MODULE inputs
                        ihb = 30.0, &
                        cd_fac = 0.7, &
                        c_alk = 1.5, &
-                       c1 = 7.57, & ! carbon dioxide
+                       c1 = 7.57e03, & ! carbon dioxide perturbed by e03
                        c2 = 1.67e03, & ! bicarboante
                        c3 = 3.15e02, & ! carbonate
                        c4 = 2.97e02, & ! B(OH)3
