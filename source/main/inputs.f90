8eebf4606db9562b98845e65cc1c8d893e6ecc31
anpa5279 <anpa5279@colorado.edu>
2025-11-28 16:24:25 -0700

    testing original run case without coriolis to see if my other run had a bug

--- source/main/inputs.f90
+++ source/main/inputs.f90
@@ -3,7 +3,7 @@ MODULE inputs
 
   ! base case for weak 20
   REAL, PARAMETER ::  ws10 = 5.75,                           &
-                      iTsurf = 25,                         &
+                      iTsurf = 25.0,                         &
                       hflux = 0.0e-7,                    &
                       ihb = 30.0,                           &
                       cd_fac = 0.7,                         &
@@ -15,7 +15,7 @@ MODULE inputs
                       c5  =  1.18909e02,             &
                       c6  =  6.30928e-03,           &
                       c7  =  9.60492,             &
-                      ustokes = 5,75,                       &
+                      ustokes = 5.75,                       &
                       Rgas = 0.0083143
 
 
