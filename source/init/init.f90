a1bf0308a078d266ee30f275ebff117924e929ff
anpa5279 <anpa5279@colorado.edu>
2025-12-01 16:40:47 -0700

    testing no fcor

--- source/init/init.f90
+++ source/init/init.f90
@@ -29,7 +29,7 @@ SUBROUTINE init
   turb_la = 0.3
   rlat    = 30
 
-  fcor    = 2.0*pi2*SIN(rlat*d_to_r)/(24.0*3600.0)
+  fcor    = 0.0 !2.0*pi2*SIN(rlat*d_to_r)/(24.0*3600.0)
   fcor_h  = 0.0
   ugcont  = 0.0
   vgcont  = 0.
