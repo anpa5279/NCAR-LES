bb96498f0f1b31970ad3fcf495390ccdde8155d3
anpa5279 <anpa5279@colorado.edu>
2025-12-16 23:04:55 -0600

    printing statements

--- source/solve/strang1.f90
+++ source/solve/strang1.f90
@@ -13,14 +13,14 @@ SUBROUTINE strang1(it)
 
   ! TEMP SCALAR ARRAY TO HOLD RHS FROM STEP N-1 AND FIELD VARIABLES FROM STEP N
   REAL :: trhs(nnx,iys:iye,nscl,izs:ize)
-  REAL, DIMENSION(nscl-1) :: tmp, c
+  REAL, DIMENSION(nscl-1) :: tmp, c_local
 
   DO iz=izs,ize
     DO iy=iys,iye
       DO ix=1,nnx
-        c(:) = t(ix,iy,2:nscl,iz)
-
-        tmp = dydt(time, c, t(ix,iy,1,iz)) !react_src(ix,iy,1,iz)
+        c_local(:) = t(ix,iy,2:nscl,iz)
+        print(c_local)
+        tmp = dydt(time, c_local, t(ix,iy,1,iz)) !react_src(ix,iy,1,iz)
         DO l=2,nscl
           trhs(ix,iy,l,iz) = tmp(l-1)
           IF(trhs(ix,iy,l,iz).le.1.0e-20)THEN
